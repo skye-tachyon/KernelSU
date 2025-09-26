0ed1b1dbfd76a913bad657c2cfac5d06163ca58c
weishu <twsxtd@gmail.com>
2025-09-26 13:42:51 +0800

    kernel: ksud_escape: security_bounded_transition for < 4.14 (tiann#1704)
    - https://github.com/torvalds/linux/commit/af63f4193f9fbbbac50fc766417d74735afd87ef
    
    - SELinux domain transitions under NNP/nosuid environment was introduced in 4.14 by
      the above commit, for older kernels, we need to make sure our domain transitions
      are allowed when calling ksud at boot from the init
    
    - Adapted from https://github.com/tiann/KernelSU/issues/270#issuecomment-2097706460
      https://github.com/tiann/KernelSU/commit/0950fbba5e79820da539054f6eb6d1d48ec96ff4
    
    - https://github.com/tiann/KernelSU/pull/1704
      https://github.com/tiann/KernelSU/commit/d664fe3d37cfb729190a26da17e3f1ccebfac4ca
    
    Difference to tiann's version:
    - use a kretprobe to force a 0 return
    - grab sids outside of kprobe context to avoid stuckups / hangups
    
    Logs:
    daisy:/ # dmesg | grep -E "transition|grab_sids"
    [    5.977810] KernelSU: ksud_grab_sids: got init sid: 62
    [    5.977907] KernelSU: ksud_grab_sids: got su sid: 537
    [    5.980497] KernelSU: kp_ksud: register kretprobe: security_bounded_transition ret: 0
    [   32.008560] KernelSU: kp_ksud: security_bounded_transition: allowing init (62) -> su (537)
    [   32.008663] type=1401 audit(2247197.199:61): op=security_bounded_transition seresult=denied oldcontext=u:r:init:s0 newcontext=u:r:su:s0
    [   36.946527] KernelSU: kp_ksud: security_bounded_transition: allowing init (62) -> su (537)
    [   61.202278] KernelSU: kp_ksud: security_bounded_transition: allowing init (62) -> su (537)
    [   61.202395] type=1401 audit(1761288080.219:1045): op=security_bounded_transition seresult=denied oldcontext=u:r:init:s0 newcontext=u:r:su:s0
    daisy:/ # uname -a
    Linux localhost 4.9.337+64-daikura/db23b17 #634 SMP PREEMPT Fri Oct 24 14:37:19 PST 2025 aarch64 Toybox
    
    old: https://github.com/backslashxx/KernelSU/commit/2cdd24b651c16ad289d83212037c73b378f22332
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/runtime/ksud_escape.h
+++ kernel/runtime/ksud_escape.h
@@ -1,7 +1,7 @@
 #ifndef __KSU_H_KSUD_ESCAPE
 #define __KSU_H_KSUD_ESCAPE
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 14, 0) && LINUX_VERSION_CODE >= KERNEL_VERSION(4, 2, 0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 14, 0) && LINUX_VERSION_CODE >= KERNEL_VERSION(4, 2, 0) && !defined(CONFIG_KRETPROBES)
 __attribute__((cold)) static noinline void sys_execve_escape_ksud_internal(const char __user **filename_user);
 __attribute__((cold)) static noinline void kernel_execve_escape_ksud_internal(void **filename_ptr);
 static inline void sys_execve_escape_ksud(const char __user **filename_user)
