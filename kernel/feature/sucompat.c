5b4d6b90d21dc19dfa00c1e51da259def2d45836
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-12-12 22:15:55 +0800

    kernel: tiny_sulog: basic ringbuffer, timestamped su log
    This is a very basic ringbuffer implementation.
    on this one we keep 8 bytes per entry and 250 entries max.
    
    This logs escalation access about: who did it, what happened and when did
    it happen.
    
    userspace can deduce as we also send next index to it. which makes it so
    that this is also the oldest entry. next -1 is the latest entry.
    
    v2: timestamped version
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/feature/sucompat.c
+++ kernel/feature/sucompat.c
@@ -99,7 +99,8 @@ check_ptr:
 
 static __always_inline int ksu_sucompat_user_common(const char __user **filename_user,
 				const char *syscall_name,
-				const bool escalate)
+				const bool escalate,
+				const uint8_t sym)
 {
 	uintptr_t buf;
 	const char su[] = SU_PATH;
@@ -160,6 +161,8 @@ static __always_inline int ksu_sucompat_user_common(const char __user **filename
 	if (unlikely(buf != su_p[0]))
 		return 0;
 
+	write_sulog(sym);
+
 	if (!escalate)
 		goto no_escalate;
 
@@ -193,7 +196,7 @@ int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
 	if (!is_su_allowed((const void **)filename_user))
 		return 0;
 
-	return ksu_sucompat_user_common(filename_user, "faccessat", false);
+	return ksu_sucompat_user_common(filename_user, "faccessat", false, 'a');
 }
 
 // sys_newfstatat, sys_fstat64
@@ -202,7 +205,7 @@ int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags)
 	if (!is_su_allowed((const void **)filename_user))
 		return 0;
 
-	return ksu_sucompat_user_common(filename_user, "newfstatat", false);
+	return ksu_sucompat_user_common(filename_user, "newfstatat", false, 's');
 }
 
 // sys_execve, compat_sys_execve
@@ -218,7 +221,7 @@ static __always_inline int ksu_handle_execve_sucompat(int *fd, const char __user
 	if (!is_su_allowed((const void **)filename_user))
 		return 0;
 
-	return ksu_sucompat_user_common(filename_user, "sys_execve", true);
+	return ksu_sucompat_user_common(filename_user, "sys_execve", true, 'x');
 }
 
 static __always_inline int ksu_sucompat_kernel_common(void **filename_ptr, void *argv, void *envp, const char *function_name)
@@ -259,6 +262,9 @@ static __always_inline int ksu_sucompat_kernel_common(void **filename_ptr, void 
 
 	if (unlikely(fn_p[0] != su_p[0]))
 		return 0;
+
+	// we only handle execve here after removing vfs_statx hook for >= 6.1
+	write_sulog('x');
 
 #ifdef CONFIG_KSU_FEATURE_SULOG
 	ksu_sulog_emit(KSU_SULOG_EVENT_SUCOMPAT, NULL, NULL, GFP_KERNEL);
