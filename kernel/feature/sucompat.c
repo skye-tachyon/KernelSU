7694ede53c9b973a737e5483ddd9983b0b5ad49b
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-01-13 14:38:27 +0800

    kernel: sucompat: execve: escape init ksud call to root
    this removes the need for that "selinux hook" on 4.9 and older
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/feature/sucompat.c
+++ kernel/feature/sucompat.c
@@ -52,6 +52,52 @@ static __always_inline bool is_su_allowed(const void **ptr_to_check)
 	return true;
 }
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 14, 0)
+__attribute__((cold))
+static noinline void sys_execve_escape_ksud(const char __user **filename_user)
+{
+	// see if its init
+	if (!is_init(current_cred()))
+		return;
+
+	const char ksud_path[] = KSUD_PATH;
+	char path[sizeof(ksud_path)];
+
+	// see if its trying to execute ksud
+	if (ksu_copy_from_user_retry(path, *filename_user, sizeof(path)))
+		return;
+
+	if (memcmp(ksud_path, path, sizeof(path)))
+		return;
+
+	pr_info("sys_execve: escape init executing ksud with pid: %d\n", current->pid);
+
+	escape_to_root_forced(); // give this context all permissions
+	
+	return;
+}
+
+__attribute__((cold))
+static noinline void kernel_execve_escape_ksud(void *filename_ptr)
+{
+	// see if its init
+	if (!is_init(current_cred()))
+		return;
+
+	if (likely(memcmp(filename_ptr, KSUD_PATH, sizeof(KSUD_PATH))))
+		return;
+
+	pr_info("kernel_execve: escape init executing ksud with pid: %d\n", current->pid);
+
+	escape_to_root_forced(); // give this context all permissions
+	
+	return;
+}
+#else
+static inline void sys_execve_escape_ksud(const char __user **filename_user) { } // no-op
+static inline void kernel_execve_escape_ksud(void *filename_ptr) { } // no-op
+#endif
+
 static noinline int ksu_sucompat_user_common(const char __user **filename_user,
 				const char *syscall_name,
 				const bool escalate)
@@ -115,6 +161,9 @@ int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags)
 static int ksu_handle_execve_sucompat(int *fd, const char __user **filename_user,
 				void *argv, void *envp, int *flags)
 {
+	if (unlikely(!ksu_boot_completed))
+		sys_execve_escape_ksud(filename_user);
+
 	if (!is_su_allowed((const void **)filename_user))
 		return 0;
 
@@ -156,6 +205,9 @@ no_escalate:
 // take note: struct filename **filename
 int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv, void *envp, int *flags)
 {
+	if (unlikely(!ksu_boot_completed))
+		kernel_execve_escape_ksud((void *)(*filename_ptr)->name);
+
 	if (!is_su_allowed((const void **)filename_ptr))
 		return 0;
 
@@ -171,6 +223,9 @@ int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr, void *
 // take note: char **filename
 int ksu_legacy_execve_sucompat(const char **filename_ptr, void *argv, void *envp)
 {
+	if (unlikely(!ksu_boot_completed))
+		kernel_execve_escape_ksud((void *)*filename_ptr);
+
 	if (!is_su_allowed((const void **)filename_ptr))
 		return 0;
 
