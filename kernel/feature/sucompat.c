938500a8a9987d56036b8130ed69622e41c6bb31
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-01-13 14:38:27 +0800

    kernel: ksud_escape: execve: escape init ksud call to root
    this removes the need for that "selinux hook" on 4.4 and 4.9
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/feature/sucompat.c
+++ kernel/feature/sucompat.c
@@ -140,6 +140,8 @@ int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags)
 // sys_execve, compat_sys_execve
 static int ksu_handle_execve_sucompat(int *fd, const char __user **filename_user, void *argv, void *envp, int *flags)
 {
+	sys_execve_escape_ksud((void *)filename_user);
+
 	if (!is_su_allowed((const void **)filename_user))
 		return 0;
 
@@ -148,6 +150,8 @@ static int ksu_handle_execve_sucompat(int *fd, const char __user **filename_user
 
 static __always_inline int ksu_sucompat_kernel_common(void **filename_ptr, void *argv, void *envp, const char *function_name)
 {
+	kernel_execve_escape_ksud((void *)filename_ptr);
+
 	if (!is_su_allowed((const void **)filename_ptr))
 		return 0;
 
