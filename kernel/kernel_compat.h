5839179eee2d675e1849b6347849fd00611274d3
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-03-12 18:28:16 +0800

    kernel: selinux/rules: backport handle_sepolicy to legacy policydb
    We do NOT have a clean way to do what upstream does where it duplicates policydb, modifies the duplicate,
    pointerswaps modified policydb, destroys old policydb.
    
    However, we can hotpatch policydb like it used to do.
    This commit adds selinux policydb access exclusivity to old kernels.
    
    if we have access to policy_rwlock, we hold it, tracking its callee, its like:
    
    Logs:
    [    2.439107] BUG: sleeping function called from invalid context at security/selinux/ss/hashtab.c:47
    [    2.439110] in_atomic(): 1, irqs_disabled(): 0, pid: 1, name: init
    [    2.439114] CPU: 7 PID: 1 Comm: init Tainted: G S      W       4.14.357+16-mojito/ae492dd #0
    [    2.439116] Hardware name: Qualcomm Technologies, Inc. SM6150 PM6150 IDP (DT)
    [    2.439118] Call trace:
    [    2.439126] dump_backtrace+0x0/0x3a0
    [    2.439133] dump_stack+0x8c/0xd0
    [    2.439136] ___might_sleep+0xf4/0x110
    [    2.439140] hashtab_insert+0x34/0x1c0
    [    2.439144] add_type+0x130/0x320
    [    2.439146] apply_kernelsu_rules+0x1b4/0x10c0
    [    2.439149] ksu_handle_initrc+0xb0/0x230
    [    2.439152] ksu_file_permission+0x24/0x40
    [    2.439155] security_file_permission+0x38/0xe0
    [    2.439158] vfs_read+0xc4/0x240
    [    2.439161] SyS_read+0xf4/0x1b0
    [    2.439164] el0_svc_naked+0x34/0x38
    [    2.467010] KernelSU: Cached su SID: 578
    --
    [    7.596782] BUG: sleeping function called from invalid context at security/selinux/ss/hashtab.c:47
    [    7.596790] in_atomic(): 1, irqs_disabled(): 0, pid: 981, name: ksud
    [    7.596796] CPU: 7 PID: 981 Comm: ksud Tainted: G S      W       4.14.357+16-mojito/ae492dd #0
    [    7.596799] Hardware name: Qualcomm Technologies, Inc. SM6150 PM6150 IDP (DT)
    [    7.596802] Call trace:
    [    7.596811] dump_backtrace+0x0/0x3a0
    [    7.596818] dump_stack+0x8c/0xd0
    [    7.596822] ___might_sleep+0xf4/0x110
    [    7.596827] hashtab_insert+0x34/0x1c0
    [    7.596832] add_type+0x130/0x320
    [    7.596835] handle_sepolicy+0x3e0/0xa60
    [    7.596838] do_set_sepolicy+0x9c/0xe0
    [    7.596843] anon_ksu_ioctl+0x3dc/0x420
    [    7.596846] do_vfs_ioctl+0xbb4/0x11b0
    [    7.596849] SyS_ioctl+0x118/0x120
    [    7.596852] el0_svc_naked+0x34/0x38
    [    7.600251] KernelSU: target type nsfs does not exist
    [    7.600259] KernelSU: sepol: cmd #19 failed, cmd=1 subcmd=1.
    
    so if we get a hold of rwlock, there will be issues with it on:
    - hashtab_insert, cond_resched() + GFP_KERNEL
    - avtab_insert_nonunique, calls avtab_insert_inode, kmem_cache_zalloc/flex_array_put_ptr w/ GFP_KERNEL
    
    So what we do is we just enable preempt while holding the lock. Yes, illegal, but it should work.
    We try to speedup/minimize time in that illegal state by jacking up the priority if its from userspace/syscall context.
    
    However, if we don't have policy_rwlock, we kinda don't have a choice.
    We go nuclear and apply policydb rules inside stop_machine.
    The largest lock on your system.
    
    oh well, this is rarely called anyway, maybe once on init and then a bunch of them at post-fs-data, nbd.
    
    another choice is doing something like this but it has its own problems: https://github.com/tiann/KernelSU/commit/2bf6d88e92203802a07330f8063a998a247cea5f
    
    upstream: https://github.com/tiann/KernelSU/pull/3268 https://github.com/tiann/KernelSU/commit/e31f84a9e1ef290bc9f689c054d7662ec91564f3
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/kernel_compat.h
+++ kernel/kernel_compat.h
@@ -8,4 +8,25 @@
 #define ksu_kernel_read_compat kernel_read
 #define ksu_kernel_write_compat kernel_write
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 12, 0)
+static inline void *ksu_kvmalloc(size_t size, gfp_t flags)
+{
+	void *buf = kmalloc(size, flags);
+	if (!buf)
+		buf = vmalloc(size);
+	
+	return buf;
+}
+
+static inline void ksu_kvfree(void *buf)
+{
+	if (is_vmalloc_addr(buf))
+		vfree(buf);
+	else
+		kfree(buf);
+}
+#define kvmalloc ksu_kvmalloc
+#define kvfree ksu_kvfree
+#endif
+
 #endif
