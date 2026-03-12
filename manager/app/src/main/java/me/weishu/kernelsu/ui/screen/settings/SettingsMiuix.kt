f034f6ee857b7e13629fc25ae0fd33bc73ad177c
KOWX712 <leecc0503@gmail.com>
2026-03-12 20:22:47 +0800

    manager: QOL
    - default to material ui
    - remove warning cards
    - 32-BIT and LEGACY working mode
    - gray out magica always
    - remove seccomp status, might be misleading since we disable seccomp for manager aye

--- manager/app/src/main/java/me/weishu/kernelsu/ui/screen/settings/SettingsMiuix.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/screen/settings/SettingsMiuix.kt
@@ -353,7 +353,7 @@ fun SettingPagerMiuix(
                                     tint = if (uiState.isLateLoadMode) colorScheme.onBackground else colorScheme.disabledOnSecondaryVariant
                                 )
                             },
-                            enabled = uiState.isLateLoadMode,
+                            enabled = false,
                             checked = uiState.autoJailbreak,
                             onCheckedChange = actions.onSetAutoJailbreak
                         )
