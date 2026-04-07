c033363cb2e7255295a3e9fe78bd6e373520adcf
KOWX712 <leecc0503@gmail.com>
2026-04-07 11:24:03 +0800

    manager: add avc spoof toggle
    * Adapt to miuix 0.9.0
    
    Signed-off-by: skye-tachyon <sonicgames772@gmail.com>

--- manager/app/src/main/java/me/weishu/kernelsu/ui/screen/settings/SettingsUiState.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/screen/settings/SettingsUiState.kt
@@ -36,6 +36,10 @@ data class SettingsUiState(
     val sulogStatus: String = "",
     val isSulogEnabled: Boolean = false,
 
+    // Avc spoof
+    val avcSpoofStatus: String = "",
+    val isAvcSpoofEnabled: Boolean = true,
+
     // Umount Modules
     val isDefaultUmountModules: Boolean = false,
 
@@ -61,6 +65,7 @@ data class SettingsScreenActions(
     val onSetKernelUmountEnabled: (Boolean) -> Unit,
     val onSetSulogEnabled: (Boolean) -> Unit,
     val onSetAdbRootEnabled: (Boolean) -> Unit,
+    val onSetAvcSpoofEnabled: (Boolean) -> Unit,
     val onSetDefaultUmountModules: (Boolean) -> Unit,
     val onSetEnableWebDebugging: (Boolean) -> Unit,
     val onSetAutoJailbreak: (Boolean) -> Unit,
