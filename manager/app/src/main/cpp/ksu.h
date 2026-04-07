6e7602fc4c628a3c42a5fffc25b0e500e70f4eca
KOWX712 <leecc0503@gmail.com>
2026-04-07 11:24:03 +0800

    manager: add avc spoof toggle
    * Adapt to miuix 0.9.0
    
    Signed-off-by: skye-tachyon <sonicgames772@gmail.com>

--- manager/app/src/main/cpp/ksu.h
+++ manager/app/src/main/cpp/ksu.h
@@ -42,6 +42,11 @@ bool set_kernel_umount_enabled(bool enabled);
 
 bool is_kernel_umount_enabled();
 
+// Avc spoof
+bool set_avc_spoof_enabled(bool enabled);
+
+bool is_avc_spoof_enabled();
+
 bool get_allow_list(struct ksu_new_get_allow_list_cmd *);
 
 inline std::pair<int, int> legacy_get_info() {
