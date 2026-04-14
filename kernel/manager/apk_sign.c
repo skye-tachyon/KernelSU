be9d11857641ad2aa2174ddea738e7fcbc1c8b19
skye-tachyon <sonicgames772@gmail.com>
2026-04-14 12:38:34 -0300

    kernel: Add Wild KSU Manager Support
    Signed-off-by: skye-tachyon <sonicgames772@gmail.com>

--- kernel/manager/apk_sign.c
+++ kernel/manager/apk_sign.c
@@ -386,5 +386,6 @@ bool is_manager_apk(char *path)
 	|| check_v2_signature(path, 0x375, "484fcba6e6c43b1fb09700633bf2fb4758f13cb0b2f4457b80d075084b26c588")  // KOWX712/KernelSU
 	|| check_v2_signature(path, 0x3e6, "79e590113c4c4c0c222978e413a5faa801666957b1212a328e46c00c69821bf7")  // KernelSU-Next/KernelSU-Next
 	|| check_v2_signature(path, 0x396, "f415f4ed9435427e1fdf7f1fccd4dbc07b3d6b8751e4dbcec6f19671f427870b")  // rsuntk/KernelSU
+	|| check_v2_signature(path, 0x381, "52d52d8c8bfbe53dc2b6ff1c613184e2c03013e090fe8905d8e3d5dc2658c2e4") // WKSU
 	);
 }
