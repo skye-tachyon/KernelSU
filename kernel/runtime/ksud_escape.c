828b70bcdd3edcd26bd21b9676e085b2a253fe2d
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-04-18 18:48:05 +0800

    kernel: ksud_escape: bprm: allow init ksud call to root for UL
    this removes the need for that "selinux hook" on 3.x kernels
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/runtime/ksud_escape.c
+++ kernel/runtime/ksud_escape.c
@@ -41,5 +41,80 @@ static noinline void kernel_execve_escape_ksud(void *filename_ptr)
 }
 #endif
 
+// UL bprm_set_creds handling
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 2, 0)
+static int bprm_set_creds_restore(void *data);
+static int (*orig_bprm_set_creds)(struct linux_binprm *bprm) = NULL;
+static int hook_bprm_set_creds(struct linux_binprm *bprm)
+{
+	if (likely(ksu_boot_completed))
+		goto bprm_set_creds;
+
+	if (!is_init(current_cred()))
+		goto bprm_set_creds;
+
+	if (!bprm->filename)
+		goto bprm_set_creds;
+
+	if (!!strcmp(bprm->filename, "/data/adb/ksud"))
+		goto bprm_set_creds;
+
+	pr_info("bprm_set_creds: allow init executing %s with pid: %d\n", bprm->filename, current->pid);
+	return 0;
+
+bprm_set_creds:
+	return orig_bprm_set_creds(bprm);
+}
+
+static int bprm_set_creds_restore(void *data)
+{
+	static bool ran_once = false;
+
+	if (ran_once)
+		return 0;
+
+	ran_once = true;
+
+	msleep(1000);
+	
+	struct security_operations *ops = (struct security_operations *)selinux_ops_addr;
+
+	if (!ops)
+		return 0;
+
+	if (!!strcmp((char *)ops, "selinux"))
+		return 0;
+
+	pr_info("%s: selinux_ops: 0x%lx .name = %s\n", __func__, (long)ops, (const char *)ops );
+
+	preempt_disable();
+	local_irq_disable();
+
+	if (orig_bprm_set_creds) {
+		pr_info("%s: restoring: 0x%lx to 0x%lx\n", __func__, (long)ops->bprm_set_creds, (long)orig_bprm_set_creds);
+		ops->bprm_set_creds = orig_bprm_set_creds;
+	}
+
+	smp_mb();
+
+	local_irq_enable();
+	preempt_enable();
+	return 0;
+}
+#endif
+
 static void ksud_escape_init() { }
-static void ksud_escape_exit() { }
+
+static void ksud_escape_exit()
+{
+	static bool already_ran = false;
+	if (already_ran)
+		return;
+
+	already_ran = true;
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 2, 0)
+	kthread_run(bprm_set_creds_restore, NULL, "unhook");
+#endif
+}
+
