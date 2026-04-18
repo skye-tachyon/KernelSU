6809c39f39af711dc713f698de155e686c112737
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-04-18 18:48:05 +0800

    kernel: ksud_escape: bprm: allow init ksud call to root for UL
    this removes the need for that "selinux hook" on 3.x kernels
    
    this can be done on LSM hack as we can veto what selinux says
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/runtime/ksud_escape.c
+++ kernel/runtime/ksud_escape.c
@@ -61,5 +61,103 @@ static noinline void kernel_execve_escape_ksud_internal(void *filename)
 }
 #endif
 
+// UL bprm_set_creds handling
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 2, 0)
+static uintptr_t selinux_ops_addr;
+static int (*orig_bprm_set_creds)(struct linux_binprm *bprm) = NULL;
+
+static int hook_bprm_set_creds(struct linux_binprm *bprm)
+{
+	if (likely(ksu_boot_completed))
+		goto bprm_set_creds;
+
+	if (!is_init(current_cred()))
+		goto bprm_set_creds;
+
+	if (!bprm->filename)
+		goto bprm_set_creds;
+
+	if (!!strcmp(bprm->filename, "/data/adb/ksud"))
+		goto bprm_set_creds;
+
+	struct task_security_struct *old_tsec = current_security();
+	struct task_security_struct *new_tsec = bprm->cred->security;
+
+	if (!(old_tsec->exec_sid))
+		goto bprm_set_creds;
+
+	// we copy what selinux was doing
+	// ref: https://elixir.bootlin.com/linux/v3.0.101/source/security/selinux/hooks.c#L1971
+
+	/* Default to the current task SID. */
+	new_tsec->sid = old_tsec->sid;
+	new_tsec->osid = old_tsec->sid;
+
+	/* Reset fs, key, and sock SIDs on execve. */
+	new_tsec->create_sid = 0;
+	new_tsec->keycreate_sid = 0;
+	new_tsec->sockcreate_sid = 0;
+
+	new_tsec->sid = old_tsec->exec_sid;
+	/* Reset exec SID on execve. */
+	new_tsec->exec_sid = 0;
+
+	pr_info("bprm_set_creds: allow init executing %s with pid: %d\n", bprm->filename, current->pid);
+	return 0;
+
+bprm_set_creds:
+	return orig_bprm_set_creds(bprm);
+}
+
+static int bprm_set_creds_restore(void *data)
+{
+	static bool ran_once = false;
+
+	if (ran_once)
+		return 0;
+
+	ran_once = true;
+
+	msleep(1000);
+	
+	struct security_operations *ops = (struct security_operations *)selinux_ops_addr;
+
+	if (!ops)
+		return 0;
+
+	if (!!strcmp((char *)ops, "selinux"))
+		return 0;
+
+	pr_info("%s: selinux_ops: 0x%lx .name = %s\n", __func__, (long)ops, (const char *)ops );
+
+	preempt_disable();
+	local_irq_disable();
+
+	if (orig_bprm_set_creds) {
+		pr_info("%s: restoring: 0x%lx to 0x%lx\n", __func__, (long)ops->bprm_set_creds, (long)orig_bprm_set_creds);
+		ops->bprm_set_creds = orig_bprm_set_creds;
+	}
+
+	smp_mb();
+
+	local_irq_enable();
+	preempt_enable();
+	return 0;
+}
+#endif
+
 static void ksud_escape_init() { }
-static void ksud_escape_exit() { }
+
+static void ksud_escape_exit()
+{
+	static bool already_ran = false;
+	if (already_ran)
+		return;
+
+	already_ran = true;
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 2, 0)
+	kthread_run(bprm_set_creds_restore, NULL, "unhook");
+#endif
+}
+
